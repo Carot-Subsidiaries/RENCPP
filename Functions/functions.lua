b3b8011333d538c03024b2b33b44f43134a46d0b
Carot <ibigwhitemen@gmail.com>
2024-08-06 17:00:11 +0200

    Update functions.lua

--- Functions/functions.lua
+++ Functions/functions.lua
@@ -1,35 +1,48 @@
 -- GET RENC WORKING --
-getgenv().getplayers = getplayers or function()
-  local players = {}
-  for _, x in pairs(game:GetService("Players"):GetPlayers()) do
-    players[x.Name] = x
-  end
-  players["LocalPlayer"] = game:GetService("Players").LocalPlayer
-  return players
+getgenv().getplayers = function()
+    local players = {}
+    for _, x in pairs(game:GetService("Players"):GetPlayers()) do
+        players[x.Name] = x
+    end
+    players["LocalPlayer"] = game:GetService("Players").LocalPlayer
+    return players
 end
 
-getgenv().getplayer = getplayer or function(name: string)
-  return not name and getplayers()["LocalPlayer"] or getplayers()[name]
+getgenv().getplayer = function(name: string): Player
+    return not name and getplayers()["LocalPlayer"] or getplayers()[name]
 end
 
-getgenv().getfps = getfps or function(suffix: boolean) -- @clipflip.rblx
-    local rawfps = game:GetService("Stats").Workspace.Heartbeat:GetValue()
-    local fpsnum = tonumber(rawfps)
-    local fps = tostring(math.round(fpsnum))
-    return not suffix and fps or fps.." fps"
+getgenv().getfps = function(): number -- credits: https://devforum.roblox.com/t/get-client-fps-trough-a-script/282631/14 idea: @clipflip.rblx
+    local RunService = game:GetService("RunService")
+    local FPS: number
+    local TimeFunction = RunService:IsRunning() and time or os.clock
+
+    local LastIteration: number, Start: number
+    local FrameUpdateTable = {}
+
+    local function HeartbeatUpdate()
+        LastIteration = TimeFunction()
+        for Index = #FrameUpdateTable, 1, -1 do
+            FrameUpdateTable[Index + 1] = FrameUpdateTable[Index] >= LastIteration - 1 and FrameUpdateTable[Index] or nil
+        end
+
+        FrameUpdateTable[1] = LastIteration
+        FPS = TimeFunction() - Start >= 1 and #FrameUpdateTable or #FrameUpdateTable / (TimeFunction() - Start)
+    end
+
+    Start = TimeFunction()
+    RunService.Heartbeat:Connect(HeartbeatUpdate)
+    task.wait(1.1)
+    return FPS
 end
 
-getgenv().getping = runanimation or function(suffix: boolean) -- @clipflip.rblx
-    local rawping = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()
-    local pingstr = rawping:sub(1,#rawping-7)
-    local pingnum = tonumber(pingstr)
-    local ping = tostring(math.round(pingnum))
-    return not suffix and ping or ping.." ms"
+getgenv().getping = function(): number -- idea: @clipflip.rblx
+    return game:GetService("Stats"):FindFirstChild("PerformanceStats").Ping:GetValue()
 end
 
-getgenv().runanimation = runanimation or function(animationId, player) -- @.wyv_
-    local plr = player or getplayer()
-    local humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
+getgenv().runanimation = function(animationId: any, player: Player) -- @.wyv_
+    local plr: Player = player or getplayer()
+    local humanoid: Humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
     if humanoid then
         local animation = Instance.new("Animation")
         animation.AnimationId = "rbxassetid://" .. tostring(animationId)
@@ -37,27 +50,15 @@ getgenv().runanimation = runanimation or function(animationId, player) -- @.wyv_
     end
 end
 
-getgenv().getdevice = getdevice or function() -- @clipflip.rblx
-    local inputsrv = game:GetService("UserInputService")
-        if inputsrv:GetPlatform() == Enum.Platform.Windows then
-            return 'Windows'
-        elseif inputsrv:GetPlatform() == Enum.Platform.OSX then
-            return 'macOS'
-        elseif inputsrv:GetPlatform() == Enum.Platform.IOS then
-            return 'iOS'
-        elseif inputsrv:GetPlatform() == Enum.Platform.UWP then
-            return 'Windows (Microsoft Store)'
-        elseif inputsrv:GetPlatform() == Enum.Platform.Android then
-            return 'Android'
-    else return 'Unknown'
-    end
+getgenv().getdevice = function(): string -- @clipflip.rblx
+    return tostring(game:GetService("UserInputService"):GetPlatform()):split(".")[3]
 end
 
-getgenv().getaffiliateid = getaffiliateid or function()
-  return "moREnc"
+getgenv().getaffiliateid = function(): string
+    return "none"
 end
 
-getgenv().customprint = customprint or function(text: string, properties: table, imageId: rbxasset)
+getgenv().customprint = function(text: string, properties: table, imageId: rbxasset) --[[ NOT RECOMMENDED ATM
     print(text)
     task.wait(.025)
     local msg = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI:WaitForChild("MainView").ClientLog[tostring(#game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI.MainView.ClientLog:GetChildren())-1].msg
@@ -65,25 +66,21 @@ getgenv().customprint = customprint or function(text: string, properties: table,
         msg[i] = x
     end
     if imageId then msg.Parent.image.Image = imageId end
+    ]]
 end
 
-getgenv().getlocalplayer = getlocalplayer or function()
+getgenv().getlocalplayer = function(): Player
     return getplayer()
 end
 
-getgenv().join = join or function(placeID: number, jobID: string)
+getgenv().join = function(placeID: number, jobID: string)
     game:GetService("TeleportService"):TeleportToPlaceInstance(placeID, jobID, getplayer())
 end
 
-local oldfiresignal
-oldfiresignal = hookfunction(firesignal, function(path: Instance, signal: string)
-    return oldfiresignal(path[signal])
-end)
-
 getgenv().firetouchtransmitter = firetouchinterest
 getgenv().playanimation = runanimation
-getgenv().getplatform = getdevice
-getgenv().getos = getdevice
+getgenv().getplatform, getgenv().getos = getdevice, getdevice
+getgenv().joingame, getgenv().joinserver = join, join
 -- GET RENC WORKING --
 
 local functions = {}
